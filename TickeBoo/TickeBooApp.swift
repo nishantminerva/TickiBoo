50b6ef819bf7bbabc822d253ebe35ceb6dca1676
Nishant Minerva <nishantcafe@gmail.com>
2023-03-28 17:12:08 +0530

    Initial Commit

---
+++ TickeBoo/TickeBooApp.swift
@@ -0,0 +1,17 @@
+//
+//  TickeBooApp.swift
+//  TickeBoo
+//
+//  Created by Nishant Minerva on 28/03/23.
+//
+
+import SwiftUI
+
+@main
+struct TickeBooApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
